294e016c7c3fe2ff56bceb2bb7d054d2014a961d
Faisalalfatoni <147465018+Faisalalfatoni@users.noreply.github.com>
2023-10-12 22:58:17 +0700

    belajar javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+// alert("Hello World");
+
+// belajar menggunakan variabel Let
+
+let namaLengkap = "Faisal Alfa Toni";
+document.write(namaLengkap);
+namaLengkap = "\n || Dendi Cahyadi\n";
+document.write(namaLengkap);
+
+// Aritmatika
+
+let x = 5;
+let y = 6;
+document.write(x);
+document.write("\n+\n");
+document.write(y);
+document.write("\n=\n");
+document.write(x + y);
+document.write("\n(Ini adalah variabel x + y yg dimana x = 5 dan y = 6)");
+
+//output menggunakan get.ElementById
+
+document.getElementById("demo").innerHTML = "Hello World";
+document.getElementById("hehe").innerHTML = "Maaf cuma bisa segini hehehe :)";
